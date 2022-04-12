265b21c5576316b7ccdc0538613fd4cdbaad79b9
joe-delaney <joseph.delaney546@gmail.com>
2022-04-12 12:05:37 -0400

    finished initial set-up -- phase 1

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    const header = <h1>Todos App</h1>;
+
+    ReactDOM.render(header, root);
+})
