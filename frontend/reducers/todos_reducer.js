c4f7b562c2a0ed99878c1f61a88c072141e896e9
joe-delaney <joseph.delaney546@gmail.com>
2022-04-12 13:54:16 -0400

    working on receiving and reducing todos

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,5 +1,33 @@
-const todosReducer = (state = {}, action) => {
+import {RECEIVE_TODOS, RECEIVE_TODO} from "../actions/todo_actions"
+
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let nextState;
     switch (action.type) {
+      case RECEIVE_TODOS:
+        nextState = {};
+        action.todos.forEach((todo) => {
+          nextState[todo.id] = todo;
+        })
+        return nextState;
+      case RECEIVE_TODO:
+        nextState = Object.assign({}, state);
+
       default:
         return state;
     }
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const header = <h1>Todos App</h1>;
-
     ReactDOM.render(header, root);
+
+    //Remove this after done developing
+    const store =  configureStore();
+    window.store =  store;
+    
 })
