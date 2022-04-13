91265e67786f128cd4bdf5d998954f2900fae422
joe-delaney <joseph.delaney546@gmail.com>
2022-04-13 14:55:47 -0400

    finished phase 4

--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -4,15 +4,24 @@ class ToDoListItem extends React.Component {
     constructor(props){
         super(props);
         this.handleDelete = this.handleDelete.bind(this);
+        this.doneClicked = this.doneClicked.bind(this);
     }
 
     handleDelete(e){
         this.props.removeTodo(this.props.todo);
     }
 
+    doneClicked(e) {
+        let temp = this.props.todo;
+        temp.done = !temp.done;
+        this.props.receiveTodo(temp);
+    }
+
     render(){
         return (
             <li>Id: {this.props.todo.id} Title: {this.props.todo.title} Body: {this.props.todo.body}
+                <br />
+                <button onClick={this.doneClicked}>{this.props.todo.done ? "Undo" : "Done"}</button>
                 <button onClick={this.handleDelete}>Delete Todo</button>
             </li>
         )
